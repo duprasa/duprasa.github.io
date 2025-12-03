29c586e4e690dab5a854e4f18d18561b04f3d50d
Sam Dupras <sam.dupras@gmail.com>
2025-12-02 22:38:08 -0500

    cleaner home page

---
+++ layouts/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="root">
+    <WaveBackground />
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.root {
+  display: flex;
+  flex-direction: row;
+  color: #ffc0cb;
+  min-height: 100vh;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-grow: 1;
+  padding: 0px 8px 100px 8px;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <script setup>
+definePageMeta({
+  layout: 'home'
+})
 const { hasThumbnail, getThumbnail } = usePostThumbnail();
 const catsQuery = {
   where: [
